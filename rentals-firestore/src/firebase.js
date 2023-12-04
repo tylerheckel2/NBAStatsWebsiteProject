75ea222be36832cd633b5d1f4d2292045c7ca3cf
Brendan Quigley <bquigs@comcast.net>
2023-12-03 20:29:43 -0500

    Added the Firebase code

---
+++ rentals-firestore/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMyv9qEjEDSgD16Mg19euXeiEsdFuxbdM",
+  authDomain: "project3-59d81.firebaseapp.com",
+  projectId: "project3-59d81",
+  storageBucket: "project3-59d81.appspot.com",
+  messagingSenderId: "491487211751",
+  appId: "1:491487211751:web:7222bf00563fc37869b242"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
