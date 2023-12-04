848ba309290fe31d87f07108b0667b02da73b0b3
Brendan Quigley <bquigs@comcast.net>
2023-12-03 20:48:23 -0500

    Made changes to home screen. Added a link

--- rentals-firestore/src/App.js
+++ rentals-firestore/src/App.js
@@ -5,21 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nba.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          NBA Tracker
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
