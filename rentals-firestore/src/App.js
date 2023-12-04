e8f24ee9609ba6f4dae990230d3f831d002c25e0
Brendan Quigley <bquigs@comcast.net>
2023-12-03 22:02:15 -0500

    Added photos of players to the folder

--- rentals-firestore/src/App.js
+++ rentals-firestore/src/App.js
@@ -5,19 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <head>
+        <title> Brendan, Tyler, Colin's Project </title>
+        <link rel="stylesheet" 
+        //href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+        crossorigin="anonymous"/>
+      </head>
         <a>
-        <div class="jumbotron">
-          <h1 class="display-3">NBA Stats Tracker</h1>
-          <p class="lead">On our website, we keep track of NBA Statistics by tracking players weekly.</p>
-          <hr class="my-2">
-          </hr>
-          <p>Our page will update at the start of every week.</p>
-          <p class="lead">
-            <a class="btn btn-primary btn-lg" href="https://nba.com" role="button">NBA.com</a>
-          </p>
-        </div>
+          <div class="jumbotron">
+            <h1 class="display-3">NBA Stats Tracker</h1>
+            <p class="lead">On our website, we keep track of NBA Statistics by tracking players weekly.</p>
+            <hr class="my-2">
+            </hr>
+            <p>Our page will update at the start of every week.</p>
+            <p class="lead">
+              <a class="btn btn-primary btn-lg" href="https://nba.com" role="button">NBA.com</a>
+            </p>
+          </div>
         </a>
-
       </header>
     </div>
   );
