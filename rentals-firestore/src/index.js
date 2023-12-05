de9ecd8c927b949a3c0fd1657f26326add1f7c48
tylerheckel2 <tylerheckel2@gmail.com>
2023-12-04 21:45:25 -0500

    created homepage.html file and rendered sign in screen

--- rentals-firestore/src/index.js
+++ rentals-firestore/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RegisterAndLogin from './registerAndLogin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RegisterAndLogin />
   </React.StrictMode>
 );
 
